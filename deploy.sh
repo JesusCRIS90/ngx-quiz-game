2f6e8ca080910c05937a791ad4cab0383aa10efe
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-09-22 17:40:47 +0200

    [ADD] Simplification Deployment Process

--- deploy.sh
+++ deploy.sh
@@ -15,24 +15,20 @@ print_error() {
   echo "\033[1;31m❌ $1\033[0m"
 }
 
-
 # Load environment variables from .env
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
+if [ -f .env-deploy ]; then
+  export $(grep -v '^#' .env-deploy | xargs)
   print_success ".env loaded"
 else
-  print_error ".env file not found!"
+  print_error ".env-deploy file not found!"
   exit 1
 fi
 
 # Ensure required env vars are set
 if [ -z "$GITHUB_USER" ] || [ -z "$REPO_NAME" ] || [ -z "$PROJECT_NAME" ]; then
-  print_error "GITHUB_USER, REPO_NAME, PROJECT_NAME must be defined in the .env file"
+  print_error "GITHUB_USER, REPO_NAME, PROJECT_NAME must be defined in the .env-deploy file"
   exit 1
 fi
-
-# # Extract Angular project name from angular.json
-# PROJECT_NAME=$(node -p "require('./angular.json').defaultProject")
 
 # Remove previous dist/ folder
 print_step "Removing previous dist/ folder if it exists"
@@ -41,14 +37,12 @@ print_success "Old dist/ folder removed"
 
 # Build the Angular project with base-href for GitHub Pages
 print_step "Building Angular project with base-href"
-# ng build --base-href "https://${GITHUB_USER}.github.io/${REPO_NAME}/"
-ng build $PROJECT_NAME --base-href "/${REPO_NAME}/" --deploy-url "/${REPO_NAME}/"
+# ng build $PROJECT_NAME -- --base-href "/${REPO_NAME}/" --deploy-url "/${REPO_NAME}/"
+ng build $PROJECT_NAME --configuration production
 print_success "Build complete"
-
 
 # Navigate to the output directory
 print_step "Navigating to dist/$PROJECT_NAME/browser"
-# cd dist/${PROJECT_NAME}
 cd dist/${PROJECT_NAME}/browser
 
 # Deploy to GitHub Pages
@@ -60,6 +54,5 @@ git commit -m 'deploy' > /dev/null
 print_success "Git commit done"
 
 print_step "Pushing to GitHub gh-pages branch"
-# git push -f git@github.com:${GITHUB_USER}/${REPO_NAME}.git main:gh-pages
 git push -f https://github.com/${GITHUB_USER}/${REPO_NAME}.git main:gh-pages
 print_success "Deployment complete!"
